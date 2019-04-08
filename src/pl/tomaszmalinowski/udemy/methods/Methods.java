c98c03187dfe150b027cca9c96884cbabaafc7fb
siekieraut <ryzy85@gmail.com>
2019-04-08 13:58:16 +0200

    next

--- src/pl/tomaszmalinowski/udemy/methods/Methods.java
+++ src/pl/tomaszmalinowski/udemy/methods/Methods.java
@@ -6,15 +6,58 @@ public class Methods {
         sayHi();
         sayHi();
         hi("Remek");
-        hi("Krzychu");
+        hi("Krzychu", "Piotr");
+        hi("remek", "krzychu", "piotr");
+
+        System.out.println(addition(9, addition(4, 3, 2)));
+
+        System.out.println(checkBy3(4));
 
     }
 
+    //metoda bezparametrowa
     private static void sayHi() {
         System.out.println("Hi Tomek");
     }
-
+/*
+//metoda parametrowa
     private static void hi (String name) {
         System.out.println("Hi " + name);
     }
+
+    private static void hi (String name, String name2) {
+        System.out.println("Hi " + name + ", " + name2);
+    }
+*/
+
+    private static void hi(String... names) {
+        String resoult = "";
+        for (String s : names) {
+            resoult = resoult + s + ",";
+        }
+        System.out.println("Hi " + resoult);
+
+    }
+
+    private static int addition(int... a) {
+        int resoult = 0;
+        for (int i : a) {
+            resoult += i;
+        }
+        return resoult;
+    }
+
+    private static boolean checkBy3(int a) {
+/*
+        if (a % 3 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+UPROSZCZENIE
+*/
+        return a % 3 == 0;
+    }
+
+    
 }
