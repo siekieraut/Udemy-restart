5e42cb126f8ad0091c14af93941c7449f5cdeb20
siekieraut <ryzy85@gmail.com>
2019-04-08 14:38:32 +0200

    sohot if

--- src/pl/tomaszmalinowski/udemy/methods/Zadanie4.java
+++ src/pl/tomaszmalinowski/udemy/methods/Zadanie4.java
@@ -2,10 +2,20 @@ package pl.tomaszmalinowski.udemy.methods;
 
 public class Zadanie4 {
     public static void main(String[] args) {
-        checkFirst()
+        System.out.println(checkFirst(11));
     }
 
-    private static long checkFirst(long a) {
+    private static boolean checkFirst(long a) {
+        boolean isFirst = true;
+        long counter = a-1;
+        for (; counter>1; counter--) {
+            if (a%counter == 0) {
+                isFirst = false;
+
+                break;
+            }
+        }
+        return  isFirst;
 
     }
 }
