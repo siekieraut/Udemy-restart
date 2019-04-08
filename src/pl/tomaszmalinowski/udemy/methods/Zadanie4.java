cd8b6db493473c66a55a9cf5ba9a7b4a8b0b672c
siekieraut <ryzy85@gmail.com>
2019-04-08 14:19:47 +0200

    sohot if

---
+++ src/pl/tomaszmalinowski/udemy/methods/Zadanie4.java
@@ -0,0 +1,11 @@
+package pl.tomaszmalinowski.udemy.methods;
+
+public class Zadanie4 {
+    public static void main(String[] args) {
+        checkFirst()
+    }
+
+    private static long checkFirst(long a) {
+
+    }
+}
