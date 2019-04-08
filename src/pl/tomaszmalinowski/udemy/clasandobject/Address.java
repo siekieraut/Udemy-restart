9aaa6d3e4e565075ea51b11cd856a29a6b751516
siekieraut <ryzy85@gmail.com>
2019-04-08 16:26:07 +0200

    sohot if

---
+++ src/pl/tomaszmalinowski/udemy/clasandobject/Address.java
@@ -0,0 +1,40 @@
+package pl.tomaszmalinowski.udemy.clasandobject;
+
+public class Address {
+    private String postOffice;
+    private String postCode;
+    private String street;
+    private String streetNumber;
+
+    public String getPostOffice() {
+        return postOffice;
+    }
+
+    public void setPostOffice(String postOffice) {
+        this.postOffice = postOffice;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+}
