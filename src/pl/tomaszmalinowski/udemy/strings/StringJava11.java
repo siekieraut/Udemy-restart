857685490a56942a42e8f67e65d47627246cc1b2
siekieraut <ryzy85@gmail.com>
2019-07-02 12:36:17 +0200

    ciąg dalszy

---
+++ src/pl/tomaszmalinowski/udemy/strings/StringJava11.java
@@ -0,0 +1,16 @@
+package pl.tomaszmalinowski.udemy.strings;
+
+public class StringJava11 {
+    public static void main(String[] args) {
+        var text = "text text";
+        System.out.println(text.repeat(5));
+        System.out.println(text.repeat(0)); //pusty string
+        System.out.println("              ".isBlank());
+        System.out.println("                ".isBlank());
+        System.out.println("                 text text               ".strip());
+        System.out.println("                 text text               ".stripLeading());
+        System.out.println("                 text text               ".stripTrailing());
+        System.out.println("on\noff\n1\n0\n".lines().count()); // policz nowe linie
+
+    }
+}
---
+++ src/pl/tomaszmalinowski/udemy/strings/Zadanie7.java
@@ -0,0 +1,14 @@
+package pl.tomaszmalinowski.udemy.strings;
+
+public class Zadanie7 {
+
+    public static void main(String[] args) {
+
+        encript();
+        decript();
+
+        
+
+        prvivate static String encript(String in)
+    }
+}
