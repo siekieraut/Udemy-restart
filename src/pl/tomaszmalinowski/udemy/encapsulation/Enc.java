60192340c46ad5ae019f6237a06d38040ff72512
siekieraut <ryzy85@gmail.com>
2019-10-30 13:29:14 +0100

    5

--- src/pl/tomaszmalinowski/udemy/encapsulation/Enc.java
+++ src/pl/tomaszmalinowski/udemy/encapsulation/Enc.java
@@ -5,9 +5,18 @@ public class Enc {
     private static int x; // widoczny w obrę bie danej klasy
     protected int y; // dostepny w klasie pakiecie i podklasie
 
-    int z; //modyfikator domyślny - dostep dla klasy i pakietu 
+    int z; //modyfikator domyślny - dostep dla klasy i pakietu
 
     public static void main(String[] args) { //publiczne dla wszystkich
 
+    }
+
+    private int getInside() {
+        InsideEnc insideEnc = new InsideEnc();
+        return insideEnc.insideZ;
+    }
+
+    class InsideEnc{
+        private int insideZ = 0;
     }
 }
