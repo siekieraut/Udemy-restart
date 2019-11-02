2523083f7a55019f9d54fab469fc64d08b5b0197
Tomek <maliniak85@op.pl>
2019-11-02 17:30:33 +0100

    12345678

--- src/pl/tomaszmalinowski/udemy/collections/Collections.java
+++ src/pl/tomaszmalinowski/udemy/collections/Collections.java
@@ -1,6 +1,7 @@
 package pl.tomaszmalinowski.udemy.collections;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class Collections {
 
@@ -16,12 +17,34 @@ public class Collections {
         Set<String> hashSet = new HashSet<>(); //set wyświetla elementy listy bez powturek
 
         treeSet.addAll(list);
-        System.out.println(treeSet);
-
-
+        System.out.println(treeSet); //porzadkuje w sposón naturalny dla zmiennej - string, alfabetycznie
 
         hashSet.addAll(list);
-        System.out.println(hashSet);
+        System.out.println(hashSet); // nie gwarantuje kolejności
+
+        Map<String, String> hashMap = new HashMap<>();
+        Map<String, String> treeMap = new TreeMap<>();
+
+        hashMap.put("pierwszy", "first");
+        hashMap.put("drugi", "secend");
+        treeMap.putAll(hashMap);
+
+        for (String str : hashMap.values()) {
+
+        }
+
+        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            entry.getKey();
+            entry.getValue();
+        }
+
+        list.stream()
+                .filter(s -> s.startsWith("a"))
+                .map(String::toUpperCase)
+                .sorted()
+                .forEach(System.out::println);
+        IntStream.range(0,10)
+                .forEach(System.out::println);
 
     }
 }
---
+++ src/pl/tomaszmalinowski/udemy/collections/zadanie/Point.java
@@ -0,0 +1,4 @@
+package pl.tomaszmalinowski.udemy.collections.zadanie;
+
+public class Point {
+}
